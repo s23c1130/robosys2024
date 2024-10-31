168764a66ddffab5be13ef4231d30838c3b68889
23C1130 <s23c1130sm@s.chibakoudai.jp>
2024-10-31 12:36:32 +0900

    [Add] テスト用シェルを追加

---
+++ test.bash
@@ -0,0 +1,27 @@
+#!/bin/bash -xv
+
+# SPDX-FileCopyrightText: 2024 Toki Makabe <s23c1130sm@s.chibakoudai.jp>
+# SPDX-License-Identifier:BSD-3-Clause
+
+ng () {
+    echo ${1}行目が違うよ
+    res=1
+}
+
+res=0
+
+### NORMAL INPUT ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
+exit $res
