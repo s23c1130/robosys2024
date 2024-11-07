e301f6cf5d9247dbd46e21d361ecfdd7b0dcd359
23C1130 <s23c1130sm@s.chibakoudai.jp>
2024-11-07 11:57:31 +0900

    [Fix]

--- test.bash
+++ test.bash
@@ -10,25 +10,18 @@ ng () {
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
-
-
-[ "${res}" = 0 ] && echo OK #通ったのが（人間に）分かるように表示
-exit $res
-
 ### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
-  　
+
 ### STRANGE INPUT ###
-out=$(echo あ | ./plus)           #計算できない値を入力してみる
-[ "$?" = 1 ]      || ng "$LINENO" #終了ステータスが1なのを確認
-[ "${out}" = "" ] || ng "$LINENO" #この行と上の行は入れ替えるとダメです
-  　                                      #（↑なぜかは考えてみましょう）
-out=$(echo | ./plus)              #なにも入力しない
-[ "$?" = 1 ]      || ng "$LINENO" #これも異常終了する
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
-  　
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 [ "$res" = 0 ] && echo OK
 exit $res
